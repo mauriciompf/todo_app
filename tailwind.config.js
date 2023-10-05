3777925f49611d2b84aac6f00b5a178205c31a11
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-05 17:10:50 -0400

    Initial files

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "bright-blue": "hsl(220, 98%, 61%)",
+        "check-background":
+          "linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)",
+        "light-theme-very-light-gray": "hsl(0, 0%, 98%)",
+        "light-theme-very-light-grayish": "hsl(236, 33%, 92%)",
+        "light-theme-light-grayish-blue": "hsl(233, 11%, 84%)",
+        "light-theme-dark-grayish-blue": "hsl(236, 9%, 61%)",
+        "light-theme-very-dark-grayish-blue": "hsl(235, 19%, 35%)",
+        "dark-theme-very-dark-blue": "hsl(235, 21%, 11%)",
+        "dark-theme-very-dark-desaturated-blue": "hsl(235, 24%, 19%)",
+        "dark-theme-light-grayish-blue": "hsl(234, 39%, 85%)",
+        "dark-theme-light-grayish-blue-hover": "hsl(236, 33%, 92%)",
+        "dark-theme-dark-grayish-blue": "hsl(234, 11%, 52%)",
+        "dark-theme-very-dark-grayish-blue": "hsl(233, 14%, 35%)",
+        "dark-theme-very-dark-grayish-blue-hover": "hsl(237, 14%, 26%)",
+      },
+      fontFamily: {
+        "josefin-sans": ["Josefin Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
