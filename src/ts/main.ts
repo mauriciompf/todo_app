e1ffc1b00a2867a0f2400ff982ef5bd5c303bec4
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-07 19:58:11 -0400

    Update main.ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,17 @@
+const addTodoInput = document.querySelector("#add-todo") as HTMLInputElement;
+const newTodoElement = document.querySelector("#new-todo") as HTMLElement;
+
+addTodoInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const inputValue = addTodoInput.value;
+
+    // newTodoElement.classList.remove("hidden");
+
+    addTodoInput.value = "";
+    e.preventDefault();
+  }
+});
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 
 `;
