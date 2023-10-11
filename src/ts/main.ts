81ab2241c79b942a1e17013826bba8cf89b13f65
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-10 21:00:48 -0400

    Update main.ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,92 +1,125 @@
-class TodoList {
-  private addTodoInput: HTMLInputElement;
-  private newTodoElement: HTMLElement;
+const addTodoInput = document.querySelector("#add-todo") as HTMLInputElement;
+const newTodoElement = document.querySelector("#new-todo") as HTMLElement;
+const todos = document.querySelector("#todos") as HTMLSpanElement;
+const clearCompletedButton = document.querySelector(
+  "#clear-completed-button",
+) as HTMLButtonElement;
 
-  constructor() {
-    this.addTodoInput = document.querySelector("#add-todo") as HTMLInputElement;
-    this.newTodoElement = document.querySelector("#new-todo") as HTMLElement;
+function createTodoElement(todoText: string): HTMLElement {
+  const todoControl = document.createElement("div");
+  todoControl.classList.add(
+    "flex",
+    "items-center",
+    "justify-between",
+    "rounded-t-md",
+    "border-b",
+    "border-light-theme-dark-grayish-blue",
+    "bg-white",
+    "px-4",
+    "py-2",
+    "shadow-2xl",
+  );
+  const span = document.createElement("span");
+  span.classList.add("sr-only");
+  span.textContent = "add new todo";
 
-    this.addTodoInput.addEventListener("keydown", (e) => this.handleAddTodo(e));
-  }
+  const inputControl = document.createElement("div");
+  inputControl.classList.add("flex", "items-center", "gap-3");
 
-  private handleAddTodo(e: KeyboardEvent): void {
-    if (e.key == "Enter") {
-      const inputText = this.addTodoInput.value;
-
-      const todoControl = this.createTodoElement(inputText);
-      this.newTodoElement.insertBefore(
-        todoControl,
-        this.newTodoElement.firstChild,
-      );
-
-      this.newTodoElement.classList.remove("hidden");
-
-      this.addTodoInput.value = "";
-      e.preventDefault();
+  const checkBoxInput = document.createElement("input");
+  checkBoxInput.classList.add(
+    "h-5",
+    "w-5",
+    "cursor-pointer",
+    "select-none",
+    "rounded-full",
+    "border",
+    "border-light-theme-very-light-grayish",
+  );
+  checkBoxInput.value = "";
+  checkBoxInput.type = "checkbox";
+  checkBoxInput.addEventListener("change", () => {
+    if (checkBoxInput.checked) {
+      newTodoInput.classList.add("line-through");
+      todos.textContent = String(Number(todos.textContent) - 1);
+    } else {
+      newTodoInput.classList.remove("line-through");
+      todos.textContent = String(Number(todos.textContent) + 1);
     }
-  }
+  });
 
-  private createTodoElement(todoText: string): HTMLElement {
-    const todoControl = document.createElement("div");
-    todoControl.classList.add(
-      "flex",
-      "items-center",
-      "justify-between",
-      "rounded-t-md",
-      "border-b",
-      "border-light-theme-dark-grayish-blue",
-      "bg-white",
-      "px-4",
-      "py-2",
-      "shadow-2xl",
-    );
-    const span = document.createElement("span");
-    span.classList.add("sr-only");
-    span.textContent = "add new todo";
+  const newTodoInput = document.createElement("input");
+  newTodoInput.classList.add(
+    "select-none",
+    "border-none",
+    "px-0",
+    "text-sm",
+    "outline-none",
+    "placeholder:text-light-theme-very-dark-grayish-blue",
+    "focus:border-none",
+    "focus:outline-none",
+    "focus:ring-0",
+  );
+  newTodoInput.value = todoText;
 
-    const inputControl = document.createElement("div");
-    inputControl.classList.add("flex", "items-center", "gap-3");
+  const removeTodoButton = document.createElement("button");
+  removeTodoButton.addEventListener("click", () => {
+    todoControl.remove();
+    todos.textContent = String(Number(todos.textContent) - 1);
 
-    const checkBoxInput = document.createElement("input");
-    checkBoxInput.classList.add(
-      "h-5",
-      "w-5",
-      "cursor-pointer",
-      "select-none",
-      "rounded-full",
-      "border",
-      "border-light-theme-very-light-grayish",
-    );
-    checkBoxInput.value = "";
-    checkBoxInput.setAttribute("type", "checkbox");
-    const newTodoInput = document.createElement("input");
-    newTodoInput.classList.add(
-      "select-none",
-      "border-none",
-      "px-0",
-      "text-sm",
-      "outline-none",
-      "placeholder:text-light-theme-very-dark-grayish-blue",
-      "focus:border-none",
-      "focus:outline-none",
-      "focus:ring-0",
-    );
-    newTodoInput.value = todoText;
+    if (newTodoElement.children.length - 2 === 0) {
+      newTodoElement.classList.add("hidden");
+    }
+  });
 
-    const button = document.createElement("button");
+  const img = document.createElement("img");
+  img.src = "./src/images/icon-cross.svg";
+  img.alt = "Remove item";
+  removeTodoButton.appendChild(img);
+  inputControl.appendChild(span);
+  inputControl.appendChild(checkBoxInput);
+  inputControl.appendChild(newTodoInput);
+  todoControl.appendChild(inputControl);
+  todoControl.appendChild(removeTodoButton);
 
-    const img = document.createElement("img");
-    img.src = "./src/images/icon-cross.svg";
-    img.alt = "Remove item";
-    button.appendChild(img);
-    inputControl.appendChild(span);
-    inputControl.appendChild(checkBoxInput);
-    inputControl.appendChild(newTodoInput);
-    todoControl.appendChild(inputControl);
-    todoControl.appendChild(button);
+  clearCompletedButton.addEventListener("click", () => {
+    if (checkBoxInput.checked) {
+      todoControl.remove();
+    }
 
-    return todoControl;
-  }
+    if (newTodoElement.children.length - 2 === 0) {
+      newTodoElement.classList.add("hidden");
+    }
+  });
+
+  return todoControl;
 }
 
-const todoList = new TodoList();
+function handleAddTodo(
+  addTodoInput: HTMLInputElement,
+  newTodoElement: HTMLElement,
+): (e: KeyboardEvent) => void {
+  return (e: KeyboardEvent) => {
+    if (e.key == "Enter") {
+      const inputText = addTodoInput.value;
+
+      const todoControl = createTodoElement(inputText);
+      newTodoElement.insertBefore(todoControl, newTodoElement.firstChild);
+
+      newTodoElement.classList.remove("hidden");
+      todos.textContent = String(newTodoElement.children.length - 2);
+
+      addTodoInput.value = "";
+      e.preventDefault();
+    }
+  };
+}
+
+function initTodoList() {
+  addTodoInput.addEventListener(
+    "keydown",
+    handleAddTodo(addTodoInput, newTodoElement),
+  );
+}
+
+initTodoList();
