d38242e5e17b04cd40f57309bc69710b2096bc81
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-12 19:59:27 -0400

    Update filter-tasks.ts

--- src/ts/filter-tasks.ts
+++ src/ts/filter-tasks.ts
@@ -1,8 +1,12 @@
-export default function filterTasks(filterType: string) {
+export default function filterTasks(
+  filterType: string,
+  filterButton: HTMLButtonElement,
+) {
+  const filterButtons = document.querySelectorAll("#filter-options button");
   const todoControls = document.querySelectorAll("#todo-control");
   todoControls.forEach((todoControl) => {
     const checkBox = todoControl.querySelector(
-      "input[type='checkbox'",
+      "input[type='checkbox']",
     ) as HTMLInputElement;
     if (
       filterType === "all" ||
@@ -12,6 +16,16 @@ export default function filterTasks(filterType: string) {
       todoControl.classList.remove("hidden");
     } else {
       todoControl.classList.add("hidden");
+    }
+  });
+
+  filterButtons.forEach((btn) => {
+    if (btn === filterButton) {
+      btn.classList.add("text-blue-500");
+      btn.classList.remove("hover:text-gray-600", "focus:text-gray-600");
+    } else {
+      btn.classList.remove("text-blue-500");
+      btn.classList.add("hover:text-gray-600", "focus:text-gray-600");
     }
   });
 }
