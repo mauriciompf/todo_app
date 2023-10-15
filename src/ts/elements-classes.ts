a901bf11bf16bdbcc933c75cf368eb9616232aef
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-15 12:54:49 -0400

    Add elements-classes.ts

---
+++ src/ts/elements-classes.ts
@@ -0,0 +1,50 @@
+export const todoControlClasses = [
+  "group",
+  "flex",
+  "items-center",
+  "justify-between",
+  "border-b",
+  "border-light-theme-dark-grayish-blue",
+  "bg-light-theme-very-light-gray",
+  "px-4",
+  "py-2",
+  "shadow-2xl",
+  "dark:bg-dark-theme-very-dark-desaturated-blue",
+];
+
+export const checkBoxInputClasses = [
+  "h-5",
+  "w-5",
+  "cursor-pointer",
+  "select-none",
+  "rounded-full",
+  "border",
+  "bg-light-theme-very-light-gray",
+  "border-light-theme-very-light-grayish",
+  "dark:bg-dark-theme-very-dark-desaturated-blue",
+  "dark:border-dark-theme-very-dark-grayish-blue",
+];
+
+export const newTodoInputClasses = [
+  "cursor-pointer",
+  "w-full",
+  "select-none",
+  "border-none",
+  "px-0",
+  "text-sm",
+  "bg-light-theme-very-light-gray",
+  "outline-none",
+  "placeholder:text-light-theme-very-dark-grayish-blue",
+  "focus:border-none",
+  "focus:outline-none",
+  "focus:ring-0",
+  "dark:bg-dark-theme-very-dark-desaturated-blue",
+  "dark:text-dark-theme-light-grayish-blue-hover",
+  "dark:caret-white",
+];
+
+export const newTodoInputCompletedClasses = [
+  "line-through",
+  "text-light-theme-dark-grayish-blue",
+  "dark:text-dark-theme-dark-grayish-blue",
+];
