83e5372c8f55ccf1e68427736df6227d18c6ce88
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-17 20:43:22 -0400

    Update main.ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,13 +2,13 @@ import getElement from "./getElement";
 import filterTasks from "./filter-tasks";
 import toggleThemeMode from "./toggle-themeMode";
 import handleAddTodo from "./handleAddTodo";
-import { totalTodos, newTodoElement } from "./createElement";
+import { totalTodos, newTodoElement } from "./createTodoElement";
 
 const filterAllButton = getElement<HTMLButtonElement>("#filter-all");
 const filterActiveButton = getElement<HTMLButtonElement>("#filter-active");
 const filterCompletedButton =
   getElement<HTMLButtonElement>("#filter-completed");
-const todos = getElement<HTMLSpanElement>("#todos");
+export const todos = getElement<HTMLSpanElement>("#todos");
 const addTodoInput = getElement<HTMLInputElement>("#add-todo");
 
 addTodoInput.addEventListener("keydown", (e) => {
